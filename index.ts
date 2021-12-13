b99d43ff8d1a20eb8821124d4a9ccb39c62b2d38
Nikolai Waerpen <nikolai.waerpen@intility.no>
2021-12-13 17:56:32 +0100

    feat: authentication

--- index.ts
+++ index.ts
@@ -1,10 +1,25 @@
-import { ApolloServer } from "apollo-server";
+import { ApolloServer, AuthenticationError } from "apollo-server";
 import { PrismaClient } from "@prisma/client";
 import { schema } from "./schema";
 
 export const prisma = new PrismaClient();
 
-const server = new ApolloServer({ schema });
+const server = new ApolloServer({
+  schema,
+  context: async ({ req, res }) => {
+    const requestUser = req.headers.from;
+
+    const user = await prisma.user.findFirst({
+      where: {
+        email: requestUser,
+      },
+    });
+
+    if (!user) throw new AuthenticationError("you are not authenticated");
+
+    return { prisma, user };
+  },
+});
 
 server
   .listen(8080)
--- schema/index.ts
+++ schema/index.ts
@@ -1,7 +1,26 @@
+import { PrismaClient } from "@prisma/client";
 import { makeSchema } from "nexus";
+import path from "path";
 import * as todo from "./todo";
 import * as user from "./user";
 
+export type ContextType = {
+  prisma: PrismaClient;
+  user: {
+    id: number;
+    name: string;
+    email: string;
+    image: string;
+  };
+};
+
 export const schema = makeSchema({
   types: [todo, user],
+  outputs: {
+    typegen: path.join(process.cwd(), "generated/nexus.ts"),
+  },
+  contextType: {
+    export: "ContextType",
+    module: path.join(process.cwd(), "schema/index.ts"),
+  },
 });
